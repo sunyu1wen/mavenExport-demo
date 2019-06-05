6f5c5302fbd1fefc6f716c5474b6964c1254ba95
Sun yuwen <630049701@qq.com>
2019-06-05 15:11:58 +0800

    export

---
+++ src/main/java/TestMain.java
@@ -0,0 +1,11 @@
+/**
+ * @author ${user}
+ * @version V1.0
+ * @ClassName: ${file_name}
+ * @Date ${date} ${time}
+ */
+public class TestMain {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
